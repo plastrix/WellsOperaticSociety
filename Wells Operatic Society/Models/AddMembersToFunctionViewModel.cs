0f694e318e96ae0e5403c11b90f58433147a6135
plastrix <nicholasbarlow1@gmail.com>
2016-10-26 16:03:21 +0100

    Added popular group tags to add member to function role

--- Wells Operatic Society/Models/AddMembersToFunctionViewModel.cs
+++ Wells Operatic Society/Models/AddMembersToFunctionViewModel.cs
@@ -12,5 +12,6 @@ namespace WellsOperaticSociety.Web.Models
         public Function Function { get; set; }
         public List<MemberRolesInShow> MemberRolesInShows { get; set; }
         public MemberRolesInShow NewMemberRolesInShow { get; set; }
+        public List<string> MostUsedGroups { get; set; }
     }
 }
